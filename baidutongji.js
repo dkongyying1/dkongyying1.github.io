4b688cd42115f0e96368c709e7aac65c0ca94895
dkongyying1 <dongying2006@sina.cn>
2017-02-08 14:03:43 +0800

    Create baidutongji.js

---
+++ baidutongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5e92882ff12c6d9a585324a0b579a21a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
